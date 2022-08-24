2b1e15d5482bafd9dc9b4395cada47582bf7d335
Ranjan Dutta <connecttoranjan@gmail.com>
2022-08-24 23:37:25 +0530

    auth not working

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/auth';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  constructor(private afAuth: AngularFireAuth) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  login() {
+    // this.afAuth.auth.signInWithRedirect(new firebase.GithubAuthProvider());
+    const googleAuthProvider = new firebase.GithubAuthProvider();
+    this.afAuth.auth.signInWithRedirect(googleAuthProvider);
   }
-
 }
